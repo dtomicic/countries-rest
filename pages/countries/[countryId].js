c2de59541e3354cc1f0f5439901bd907f7ebb1d9
Darko <dtomicic192@gmail.com>
2022-10-24 13:28:36 +0200

    filters done

--- pages/countries/[countryId].js
+++ pages/countries/[countryId].js
@@ -2,63 +2,69 @@ import countryStyles from '../../styles/Page.module.css'
 import { AiOutlineArrowLeft } from "react-icons/ai";
 import Image from 'next/image'
 import Link from 'next/link'
+import Head from 'next/head'
 
 export default function Country({ country, theme }) {
     const array = Object.keys(country);
     return (
-        <div className={countryStyles.country}>
-            <Link href='/'>
-                <a>
-                    <div className={`${countryStyles["countryBack"]} ${theme ? countryStyles["countryBackDark"] : countryStyles["countryBackLight"]}`}>
-                        <AiOutlineArrowLeft />
-                        <h3 className={countryStyles.countryBackText}>Back</h3>
-                    </div>
-                </a>
-            </Link>
-            <div className={`${countryStyles["countryMain"]} ${theme ? countryStyles["countryMainDark"] : countryStyles["countryMainLight"]}`}>
-                <div className={countryStyles.countryMainImage}>
-                    <Image src={country.flags.svg} width="100%" height="50%" layout="responsive" objectFit="cover" />
-                </div>
-                <div className={countryStyles.countryMainText}>
-                    <div className={countryStyles.countryMainTextHeader}>
-                        <h3 className={countryStyles.countryMainTextHeaderName}>{country.name.official}</h3>
-                    </div>
-                    <div className={countryStyles.countryMainTextInfo}>
-                        <div className={countryStyles.countryMainTextInfoFirst}>
-                            <h3 className={countryStyles.countryMainTextInfoItem}><span className={countryStyles.countryMainTextInfoBold}>Population: </span>{(country.population).toLocaleString('en-US')}</h3>
-                            <h3 className={countryStyles.countryMainTextInfoItem}><span className={countryStyles.countryMainTextInfoBold}>Region: </span>{country.region}</h3>
-                            <h3 className={countryStyles.countryMainTextInfoItem}><span className={countryStyles.countryMainTextInfoBold}>Sub Region: </span>{country.subregion}</h3>
-                            <h3 className={countryStyles.countryMainTextInfoItem}><span className={countryStyles.countryMainTextInfoBold}>Capital: </span>{country.capital}</h3>
+        <>
+            <Head>
+                <title>{country.name.official}</title>
+            </Head>
+            <div className={countryStyles.country}>
+                <Link href='/'>
+                    <a>
+                        <div className={`${countryStyles["countryBack"]} ${theme ? countryStyles["countryBackDark"] : countryStyles["countryBackLight"]}`}>
+                            <AiOutlineArrowLeft />
+                            <h3 className={countryStyles.countryBackText}>Back</h3>
                         </div>
-                        <div className={countryStyles.countryMainTextInfoSecond}>
-                            <h3 className={countryStyles.countryMainTextInfoItem}><span className={countryStyles.countryMainTextInfoBold}>Top Level Domain: </span>{array.includes('tld') ? country.tld : 'No TLD'}</h3>
-                            <h3 className={countryStyles.countryMainTextInfoItem}><span className={countryStyles.countryMainTextInfoBold}>Currencies: </span>{array.includes('currencies') ? Object.values(country.currencies).map((currency) => {
-                                return (
-                                    <span key={currency.name}>{currency.name}</span>
-                                )
-                            }) : 'No currency'}</h3>
-                            <h3 className={countryStyles.countryMainTextInfoItem}><span className={countryStyles.countryMainTextInfoBold}>Languages: </span>{Object.values(country.languages).map((language, index) => {
-                                return (
-                                    <span key={language}>{(index ? ', ' : '') + language}</span>
-                                )
-                            })}</h3>
-                        </div>
+                    </a>
+                </Link>
+                <div className={`${countryStyles["countryMain"]} ${theme ? countryStyles["countryMainDark"] : countryStyles["countryMainLight"]}`}>
+                    <div className={countryStyles.countryMainImage}>
+                        <Image src={country.flags.svg} width="100%" height="50%" layout="responsive" objectFit="cover" />
                     </div>
-                    <div className={countryStyles.countryMainTextBorder}>
-                        <h3 className={countryStyles.countryMainTextBorderHeader}>Border Countries: </h3>
-                        <div className={countryStyles.countryMainTextBorderList}>
-                            {array.includes('borders') ? country.borders.map((country) => {
-                                return (
-                                    <div className={`${countryStyles["countryMainTextBorderBox"]} ${theme ? countryStyles["countryMainTextBorderBoxDark"] : countryStyles["countryMainTextBorderBoxLight"]}`} key={country}>
-                                        <h3 className={countryStyles.countryMainTextBorderBoxHeader}>{country}</h3>
-                                    </div>
-                                )
-                            }) : `Country doesen't have any borders`}
+                    <div className={countryStyles.countryMainText}>
+                        <div className={countryStyles.countryMainTextHeader}>
+                            <h3 className={countryStyles.countryMainTextHeaderName}>{country.name.official}</h3>
+                        </div>
+                        <div className={countryStyles.countryMainTextInfo}>
+                            <div className={countryStyles.countryMainTextInfoFirst}>
+                                <h3 className={countryStyles.countryMainTextInfoItem}><span className={countryStyles.countryMainTextInfoBold}>Population: </span>{(country.population).toLocaleString('en-US')}</h3>
+                                <h3 className={countryStyles.countryMainTextInfoItem}><span className={countryStyles.countryMainTextInfoBold}>Region: </span>{country.region}</h3>
+                                <h3 className={countryStyles.countryMainTextInfoItem}><span className={countryStyles.countryMainTextInfoBold}>Sub Region: </span>{country.subregion}</h3>
+                                <h3 className={countryStyles.countryMainTextInfoItem}><span className={countryStyles.countryMainTextInfoBold}>Capital: </span>{country.capital}</h3>
+                            </div>
+                            <div className={countryStyles.countryMainTextInfoSecond}>
+                                <h3 className={countryStyles.countryMainTextInfoItem}><span className={countryStyles.countryMainTextInfoBold}>Top Level Domain: </span>{array.includes('tld') ? country.tld : 'No TLD'}</h3>
+                                <h3 className={countryStyles.countryMainTextInfoItem}><span className={countryStyles.countryMainTextInfoBold}>Currencies: </span>{array.includes('currencies') ? Object.values(country.currencies).map((currency) => {
+                                    return (
+                                        <span key={currency.name}>{currency.name}</span>
+                                    )
+                                }) : 'No currency'}</h3>
+                                <h3 className={countryStyles.countryMainTextInfoItem}><span className={countryStyles.countryMainTextInfoBold}>Languages: </span>{Object.values(country.languages).map((language, index) => {
+                                    return (
+                                        <span key={language}>{(index ? ', ' : '') + language}</span>
+                                    )
+                                })}</h3>
+                            </div>
+                        </div>
+                        <div className={countryStyles.countryMainTextBorder}>
+                            <h3 className={countryStyles.countryMainTextBorderHeader}>Border Countries: </h3>
+                            <div className={countryStyles.countryMainTextBorderList}>
+                                {array.includes('borders') ? country.borders.map((country) => {
+                                    return (
+                                        <div className={`${countryStyles["countryMainTextBorderBox"]} ${theme ? countryStyles["countryMainTextBorderBoxDark"] : countryStyles["countryMainTextBorderBoxLight"]}`} key={country}>
+                                            <h3 className={countryStyles.countryMainTextBorderBoxHeader}>{country}</h3>
+                                        </div>
+                                    )
+                                }) : `Country doesen't have any borders`}
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
